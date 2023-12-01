3b5118c25afe179a5c55482487b2d5400b38215a
Michael de Hoog <michael.dehoog@coinbase.com>
2023-11-30 16:22:48 -0800

    Enable p256 in cancun for testing purposes

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -106,6 +106,7 @@ var PrecompiledContractsCancun = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{8}):    &bn256PairingIstanbul{},
 	common.BytesToAddress([]byte{9}):    &blake2F{},
 	common.BytesToAddress([]byte{0x0a}): &kzgPointEvaluation{},
+	common.BytesToAddress([]byte{19}):   &p256Verify{},
 }
 
 // PrecompiledContractsBLS contains the set of pre-compiled Ethereum
